dfcd23efb484231ce4be8a7b61e6d177bc199702
stephyswe <stephaniiehallberg@gmail.com>
2018-09-29 00:25:28 +0200

    Update Account Settings

--- app/src/main/java/com/example/steph/socialapp/SettingsActivity.java
+++ app/src/main/java/com/example/steph/socialapp/SettingsActivity.java
@@ -1,11 +1,18 @@
 package com.example.steph.socialapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -13,6 +20,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -43,7 +52,7 @@ public class SettingsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         userName = findViewById(R.id.settings_username);
-        userProfileName = findViewById(R.id.settings_username);
+        userProfileName = findViewById(R.id.settings_full_name);
         userStatus = findViewById(R.id.settings_status);
         userCountry = findViewById(R.id.settings_country);
         userGender = findViewById(R.id.settings_gender);
@@ -87,9 +96,80 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+        UpdateAccountSettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ValidateAccountInformation();
+            }
+        });
 
 
+    }
+
+    private void ValidateAccountInformation() {
+
+        String username = userName.getText().toString();
+        String profilename = userProfileName.getText().toString();
+        String status = userStatus.getText().toString();
+        String dob = userDOB.getText().toString();
+        String country = userCountry.getText().toString();
+        String gender = userGender.getText().toString();
+        String relation = userRelation.getText().toString();
+
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(this, "Please write your username", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(profilename)) {
+            Toast.makeText(this, "Please write your profile name", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(status)) {
+            Toast.makeText(this, "Please write your status", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(dob)) {
+            Toast.makeText(this, "Please write your date of birth", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(country)) {
+            Toast.makeText(this, "Please write your country", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(gender)) {
+            Toast.makeText(this, "Please write your gender", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(relation)) {
+            Toast.makeText(this, "Please write your relationship status", Toast.LENGTH_SHORT).show();
+        } else {
+            UpdateAccountInfo(username, profilename, status, dob,
+                    country, gender, relation);
+
+        }
 
 
+    }
+
+    private void UpdateAccountInfo (String username, String profilename, String status, String dob,
+                                    String country, String gender, String relation){
+
+        HashMap userMap = new HashMap();
+        userMap.put("username", username);
+        userMap.put("fullname", profilename);
+        userMap.put("status", status);
+        userMap.put("dob", dob);
+        userMap.put("country", country);
+        userMap.put("gender", gender);
+        userMap.put("relationship", relation);
+
+        SettingsUserRef.updateChildren(userMap).addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                if (task.isSuccessful()) {
+                    SendUserToMainActivity();
+                    Toast.makeText(SettingsActivity.this, "Account Settings Updated Successfully..", Toast.LENGTH_SHORT).show();
+                } else {
+                    String message = task.getException().getMessage();
+                    Toast.makeText(SettingsActivity.this, "Error Occured, while updating information. More info: " + message, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void SendUserToMainActivity()
+    {
+        Intent mainIntent = new Intent(SettingsActivity.this, MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
     }
 }
