839a346f1a62eafb567c8f72c34d56bb52d38816
stephyswe <stephaniiehallberg@gmail.com>
2018-09-29 16:47:00 +0200

    Find Friends - Search Filter

--- app/src/main/java/com/example/steph/socialapp/FindFriendsActivity.java
+++ app/src/main/java/com/example/steph/socialapp/FindFriendsActivity.java
@@ -1,23 +1,112 @@
 package com.example.steph.socialapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class FindFriendsActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private ImageButton SearchButton;
+    private EditText SearchInputText;
+
+    private RecyclerView SearchResultList;
+
+    private DatabaseReference allUsersDatabaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_friends);
 
+        allUsersDatabaseRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
         mToolbar = findViewById(R.id.find_friends_appbar_layout);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Find Friends");
 
+        SearchResultList = findViewById(R.id.search_result_list);
+        SearchResultList.setHasFixedSize(true);
+        SearchResultList.setLayoutManager(new LinearLayoutManager(this));
 
+        SearchButton = findViewById(R.id.search_people_friends_button);
+        SearchInputText = findViewById(R.id.search_box_input);
+
+
+        SearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = SearchInputText.getText().toString();
+                SearchPeopleAndFriends(input);
+            }
+        });
+    }
+
+    private void SearchPeopleAndFriends(String searchBoxInput) {
+
+        Toast.makeText(this, "Searching..", Toast.LENGTH_LONG).show();
+
+        Query searchPeopleAndFriendsQuery = allUsersDatabaseRef.orderByChild("fullname")
+                .startAt(searchBoxInput).endAt(searchBoxInput + "\uf8ff");
+
+        FirebaseRecyclerAdapter<FindFriends, FindFriendsViewHolder> firebaseRecyclerAdapter
+                = new FirebaseRecyclerAdapter<FindFriends, FindFriendsViewHolder>
+                (
+                        FindFriends.class,
+                        R.layout.all_users_display_layout,
+                        FindFriendsViewHolder.class,
+                        searchPeopleAndFriendsQuery
+                )
+        {
+            @Override
+            protected void populateViewHolder(FindFriendsViewHolder viewHolder, FindFriends model, int position) {
+                viewHolder.setFullname(model.getFullname());
+                viewHolder.setStatus(model.getStatus());
+                viewHolder.setProfileimage(getApplicationContext(), model.getProfileimage());
+            }
+        };
+
+        SearchResultList.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    public static class FindFriendsViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+        public FindFriendsViewHolder(View itemView) {
+            super(itemView);
+            this.mView = itemView;
+        }
+
+        public void setProfileimage(Context ctx, String profileimage) {
+            CircleImageView myImage = mView.findViewById(R.id.all_users_profile_image);
+            Picasso.with(ctx).load(profileimage).placeholder(R.drawable.profile).into(myImage);
+        }
+
+        public void setFullname(String fullname) {
+            TextView myName = mView.findViewById(R.id.all_users_profile_name);
+            myName.setText(fullname);
+        }
+
+        public void setStatus(String status) {
+            TextView myStatus = mView.findViewById(R.id.all_users_profile_status);
+            myStatus.setText(status);
+        }
     }
 }
